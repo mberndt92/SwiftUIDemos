5e0de39606d585d06b51e81be2a7f4f0f9f12cd0
Mberndt92 <maximilian_berndt@icloud.com>
2023-05-22 16:01:43 +0900

    Initial Commit

---
+++ ProSwiftUIProject/ProSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProSwiftUIProjectApp.swift
+//  ProSwiftUIProject
+//
+//  Created by Maximilian Berndt on 22.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ProSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
